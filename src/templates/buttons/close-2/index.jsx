32668547d349e368c5c30f84bc302ba8688aab3c
Eduardo Henrique Rotundaro <eduardorotundaro@gmail.com>
2019-04-07 19:23:28 -0300

    Close 2

---
+++ src/templates/buttons/close-2/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import './component.css'
+
+export default props => (
+    <div className="menu-icon">
+        <div className="line top"></div>
+        <div className="line right"></div>
+        <div className="line bottom"></div>
+        <div className="line left"></div>
+    </div>
+)
